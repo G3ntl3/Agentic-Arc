cb5b73c8e57568f5c3d3c16a9782efa7b0d73be9
g3ntl3 <hammedlawal412@gmail.com>
2025-08-19 17:30:00 +0100

    battling with Hero sect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,18 +13,26 @@ const Hero = () => {
           className="bg-cover bg-center bg-no-repeat w-[100%] mt-5 opacity-20  overflow-hidden lg:h-screen "
           style={{ backgroundImage: `url(${bgimg})` }}
         ></div>
-        <div className="  absolute inset-0 bg-emerald-950/50  ">
+        <div className=" items-center  absolute inset-0 bg-emerald-950/50  ">
           <Topnav className=" z-40 absolute  text-white" />
-          <div className="items-center">
-            <div className=" gradient-text bg-gradient-to-r from-[#00BFA6] to-[#016179] border text-6xl px-20 text-center">
-              Telling the Stories of the AI Agents  
-            </div>
-            <div className=" gradient-text bg-gradient-to-r from-[#00BFA6] to-[#016179] border text-6xl px-20 text-center">
+
+          {/* centered text  */}
+
+          <div className="absolute inset-0   flex items-center justify-center flex-col">
+            <div className="text-center space-y-4">
+              <div className="gradient-text bg-gradient-to-r from-[#00BFA6] to-[#016179] text-6xl px-20 font-bold glowtext">
+                Telling the Stories of the AI Agents
+              </div>
+              <div className="glowtext gradient-text bg-gradient-to-r from-[#00BFA6] to-[#016179] text-6xl px-20">
                 Shaping the Future
+              </div>
             </div>
-            <h1 className="text-4xl gradient-text font-bold bg-gradient-to-r from-[#ca3f15] to-[#557901] bg-clip-text text-transparent">
-              Gradient Text
-            </h1>
+            <div className="text-[36px] text-white px-30 mt-10 text-center">
+              Making AI projects simple and connecting creators with
+            </div>{" "}
+            <div className="text-[36px] text-white px-30 text-center">
+              their audience through powerful storytelling.
+            </div>{" "}
           </div>
         </div>
       </div>
